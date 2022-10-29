9d1c25d3f65d2ce508d2b27aac3e34fef97a4169
niyasudheen <niyasudheen.karimbakndy@nissanmotor.com>
2022-10-29 16:56:33 +0530

    angular

---
+++ LMS/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminlistingComponent } from './admin/adminlisting/adminlisting.component';
+import { AdminuserviewComponent } from './admin/adminuserview/adminuserview.component';
+import { AdminloanofficerviewComponent } from './admin/adminloanofficerview/adminloanofficerview.component';
+import { UserComponent } from './user/user.component';
+import { UserentryComponent } from './user/userentry/userentry.component';
+import { UserlistingComponent } from './user/userlisting/userlisting.component';
+import { LoanofficerComponent } from './loanofficer/loanofficer.component';
+import { SharedComponent } from './shared/shared.component';
+import { RoleComponent } from './shared/role/role.component';
+import { LoanComponent } from './shared/loan/loan.component';
+import { LoginComponent } from './login/login.component';
+import { LoanapplicationComponent } from './user/loanapplication/loanapplication.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    AdminlistingComponent,
+    AdminuserviewComponent,
+    AdminloanofficerviewComponent,
+    UserComponent,
+    UserentryComponent,
+    UserlistingComponent,
+    LoanofficerComponent,
+    SharedComponent,
+    RoleComponent,
+    LoanComponent,
+    LoginComponent,
+    LoanapplicationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ LMS/src/app/user/userlisting/userlisting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userlisting',
+  templateUrl: './userlisting.component.html',
+  styleUrls: ['./userlisting.component.scss']
+})
+export class UserlistingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
