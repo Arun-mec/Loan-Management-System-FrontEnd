a2b8a7cde46e0118b796bba8d1a9cc613e53a1ae
arun <arunalakkalpalla@gmail.com>
2022-10-31 15:25:29 +0530

    admin view done

--- LMS/src/app/app-routing.module.ts
+++ LMS/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
 import { UserentryComponent } from './user/userentry/userentry.component';
 
@@ -7,6 +8,7 @@ const routes: Routes = [
   {path: '', redirectTo:'/userlogin',pathMatch:'full'},
   {path: 'userentry', component: UserentryComponent},
   {path: 'userlogin', component: LoginComponent},
+  {path: 'admin', component: AdminComponent},
 ];
 
 @NgModule({
--- LMS/src/app/app.module.ts
+++ LMS/src/app/app.module.ts
@@ -11,9 +11,6 @@ import { UserComponent } from './user/user.component';
 import { UserentryComponent } from './user/userentry/userentry.component';
 import { UserlistingComponent } from './user/userlisting/userlisting.component';
 import { LoanofficerComponent } from './loanofficer/loanofficer.component';
-import { SharedComponent } from './shared/shared.component';
-import { RoleComponent } from './shared/role/role.component';
-import { LoanComponent } from './shared/loan/loan.component';
 import { LoginComponent } from './login/login.component';
 import { LoanapplicationComponent } from './user/loanapplication/loanapplication.component';
 import { CustomerentryComponent } from './user/userentry/customerentry/customerentry.component';
@@ -30,9 +27,6 @@ import { AdminentryComponent } from './user/userentry/adminentry/adminentry.comp
     UserentryComponent,
     UserlistingComponent,
     LoanofficerComponent,
-    SharedComponent,
-    RoleComponent,
-    LoanComponent,
     LoginComponent,
     LoanapplicationComponent,
     CustomerentryComponent,
