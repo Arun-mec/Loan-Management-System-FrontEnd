429379ca1ef4f702d8cf13f1026c48cc3eb4d2a8
arun <arunalakkalpalla@gmail.com>
2022-10-31 15:27:01 +0530

    admin view done

--- LMS/src/app/app-routing.module.ts
+++ LMS/src/app/app-routing.module.ts
@@ -3,13 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
 import { UserentryComponent } from './user/userentry/userentry.component';
-import { AdminentryComponent } from './admin/adminentry/adminentry.component';
-import { CustomerentryComponent } from './customer/customerentry/customerentry.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'/userlogin',pathMatch:'full'},
   {path: 'userentry', component: UserentryComponent},
   {path: 'userlogin', component: LoginComponent},
+  {path:'admin',component:AdminComponent}
 ];
 
 @NgModule({
